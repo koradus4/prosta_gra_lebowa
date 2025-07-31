7822ffe6a8c6fb5ddc7778883c0995d0f31d40e4
koradus4 <konradborkowski25@gmail.com>
2025-07-31 23:05:43 +0200

    Aktualizacja projektu

--- backup/export_to_github.py
+++ backup/export_to_github.py
@@ -17,8 +17,13 @@ def export_to_github(repo_path, branch="main", commit_message="Aktualizacja proj
         # Commit zmian
         subprocess.run(["git", "commit", "-m", commit_message], check=True)
 
-        # Ustawienie zdalnego repozytorium, jeśli podano
-        if remote_url:
+        # Sprawdzenie, czy zdalne repozytorium już istnieje
+        remote_check = subprocess.run(["git", "remote", "get-url", "origin"], capture_output=True, text=True)
+        if remote_check.returncode == 0:
+            # Aktualizacja URL zdalnego repozytorium
+            subprocess.run(["git", "remote", "set-url", "origin", remote_url], check=True)
+        else:
+            # Dodanie zdalnego repozytorium, jeśli nie istnieje
             subprocess.run(["git", "remote", "add", "origin", remote_url], check=True)
 
         # Wypchnięcie zmian na zdalne repozytorium
